894341f11da25fb0d3ffb4eb294de39d9a45a3fe
tpetric7 <50740318+tpetric7@users.noreply.github.com>
2022-03-20 16:11:50 +0100

    deleted files

--- ref.R
+++
@@ -1,6 +0,0 @@
-@article{levshina2015linguistics,
-  title={How to do linguistics with R},
-  author={Levshina, Natalia},
-  journal={Data Exploration and Statistical Analysis, Amsterdam-Philadelphia},
-  year={2015}
-}
